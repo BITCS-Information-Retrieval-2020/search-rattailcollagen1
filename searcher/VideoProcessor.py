dea0038556c30c901b1892d52aada8c7d9beed7e
wenzihan <45282468+wenzihan@users.noreply.github.com>
2020-12-31 19:50:39 +0800

    Update VideoProcessor.py
    video2text

--- searcher/VideoProcessor.py
+++ searcher/VideoProcessor.py
@@ -1,12 +1,16 @@
 import os
+import json
+import operator
 from moviepy.editor import VideoFileClip
 from aip import AipSpeech
 from pydub import AudioSegment
 from pydub.silence import detect_nonsilent
 
-APP_ID = '23399411'
-API_KEY = 'hbczxy8HWuf0G98IPq8RW5eu'
-SECRET_KEY = 'VDpW44pGMusAZ54uyzzGGDFgPRjhcnhd'
+APP_ID = '23396576'
+API_KEY = 'K5u0EndzcGigK888uQr5oQ5R'
+SECRET_KEY = 'ep66s5mpcLm7MOaZBsuEbFk53vu7Z4jm'
+
+
 
 
 def split_on_silence(audio_seg, min_silence_len, thresh, keep_sil):
@@ -88,10 +92,10 @@ def stt(audio_address):
     aipSpeech = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
 
     audio = get_file_content(audio_address)
-    json = aipSpeech.asr(audio, 'wav', 16000, {
+    jsonn = aipSpeech.asr(audio, 'wav', 16000, {
             'dev_pid': lang,
     })
-    return json.get(['result'][0])
+    return jsonn.get(['result'][0])
 
 
 def videoToSpeech(source, target):
@@ -114,6 +118,52 @@ class VideoProcessor:
     def __init__(self):
         pass
 
+
+    def video2text(self,videos_path):
+        #创建json文件夹
+        dir = videos_path + 'struct'
+        if os.path.exists(dir):
+            files = os.listdir(dir)
+            for file in files:
+                os.remove(os.path.join(dir, file))
+            os.removedirs(dir)
+        os.makedirs(dir)
+        
+        video_files = os.listdir(videos_path)
+        for video_file in video_files:
+            
+            if operator.eq(video_file.split('.')[1],'mp4') is False:
+                continue
+            video_path = os.path.join(videos_path,video_file)
+            videoToSpeech(video_path, target="tmp.wav")
+            if os.path.exists('splits'):
+                files = os.listdir('splits')
+                for file in files:
+                    os.remove('./splits/' + file)
+                os.removedirs('splits')
+            os.makedirs("splits")
+            chunks, chunk_lens = speech_split("tmp.wav", "splits")
+            lastend = 0
+            text_list = list()
+            for i, each in enumerate(chunks):
+                start = convert_time(max(int(chunk_lens[i][0]), lastend))
+                end = convert_time(int(chunk_lens[i][1]))
+                lastend = int(chunk_lens[i][1])
+                sentence = stt("./splits" + each)
+                os.remove("splits/" + each)
+                if sentence is None:
+                    continue
+                tmp = {'timeStart': start, 'timeEnd': end,  'sentence': sentence}
+                text_list.append(tmp)
+            os.remove("tmp.wav")
+            os.removedirs("splits")
+            #将list存进json文件中
+            print(text_list)
+            json_path = os.path.join(dir, video_file.split('.')[0]+'.json')
+            with open(json_path,'w') as file_obj:
+                json.dump(text_list, file_obj)
+            
+
     def convert(self, video_path):
         '''
             将一个video转换为一系列的文本
@@ -130,7 +180,15 @@ class VideoProcessor:
                         is observed in the process."
                     }
         '''
-
+        base_name = os.path.basename(video_path)
+        dir = os.path.join(os.path.abspath(os.path.join(video_path,'..','..')),'videosstruct')
+        json_path = os.path.join(dir, base_name.split('.')[0]+'.json')
+        print(json_path)
+        if os.path.exists(json_path):
+            with open(json_path) as fp:
+                json_data = json.load(fp)
+                return json_data
+                
         videoToSpeech(video_path, target="tmp.wav")
         if os.path.exists('splits'):
             files = os.listdir('splits')
@@ -155,3 +213,4 @@ class VideoProcessor:
         os.remove("tmp.wav")
         os.removedirs("splits")
         return text_list
+
