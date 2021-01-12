c96a9fd05b5743ad9cf89f42e649ebb50848ff1d
brokendream <1085639628@qq.com>
2021-01-12 17:55:10 +0800

    add example

---
+++ example.py
@@ -0,0 +1,24 @@
+from searcher.SearchEngine import SearchEngine
+
+# initialization
+se = SearchEngine(download_server_ip='xxx.xxx.xxx.xxx',
+                  download_server_port=5000,
+                  download_client_ip='xxx.xxx.xxx.xxx',
+                  download_client_port=9001,
+                  es_ip='xxx.xxx.xxx.xxx',
+                  es_port=9200,
+                  index_name='xxx',
+                  video_index_name='xxx')
+
+# search
+query = {}
+res = se.search(query)
+
+# search by id
+id = 1
+res = se.search_by_id(id)
+
+# auto-complete
+query = "xxx"
+res = se.auto_complete(query)
+
