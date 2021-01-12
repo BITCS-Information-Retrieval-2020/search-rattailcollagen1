9342e61d7f9c6866efe42ff1922f6f08bb2a612a
jerrrynie <815398117@qq.com>
2021-01-12 17:19:12 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="searcher",
+    version="1.0.0",
+    author="RatTailCollagen1",
+    author_email="815398117@qq.com",
+    description="an IR system for scientific research papers",
+    url="https://github.com/BITCS-Information-Retrieval-2020/search-rattailcollagen1",
+    packages=setuptools.find_packages(),
+    classifiers=["Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 ],
+)
